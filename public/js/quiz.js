e3592aaed814976d0c868511b323c974054537c2
Charly Whitlow <charlywhitlow@gmail.com>
2021-01-15 17:20:26 +0000

    created basic html pages and app navigation

---
+++ public/js/quiz.js
@@ -0,0 +1,26 @@
+
+function playSound(){
+    document.getElementById("reveal-bird-button").style.display = "block";
+}
+function revealBird(){
+    document.getElementById("listen").style.display = "none";
+    document.getElementById("quiz-answer").style.display = "block";
+    document.getElementById("reveal-bird-button").style.display = "none";
+
+    // show 'next' button- unless moving to bird 10 (last bird)
+    let page = parseInt(document.getElementById("page").innerHTML, 10);
+    if (page == 10) {
+        console.log("last bird done- show finish");
+        document.getElementById("next-button").style.display = "none";
+        document.getElementById("finish-block").style.display = "block";
+    }
+};
+function nextBird(){
+    document.getElementById("listen").style.display = "block";
+    document.getElementById("quiz-answer").style.display = "none";
+    document.getElementById("reveal-bird-button").style.display = "none";
+    
+    // increment page- up to page 10 (last bird)
+    let page = parseInt(document.getElementById("page").innerHTML, 10);
+    document.getElementById("page").innerHTML = page+1;
+}

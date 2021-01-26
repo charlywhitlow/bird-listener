3786ee3aa707802bf73b5a88276752bb2b3079df
Charly Whitlow <charlywhitlow@gmail.com>
2021-01-26 16:12:03 +0000

    updated birdModel to include new db fields

--- models/birdModel.js
+++ models/birdModel.js
@@ -1,7 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+let bird_json = {
+  "nbn_guid":"NHMSYS0000530119",
+  "scientific_name":"Accipiter gentilis",
+  "family":"Accipitridae",
+  "common_name":"Goshawk",
+  "other_common_names":[
+    "Northern Goshawk",
+  ],
+  "habitats":[
+    "terrestrial"
+  ],
+  "establishment_means":"Non-native",
+  "establishment_status":"GB Establishment Status - Established"
+}
+
 const BirdSchema = new Schema({
+  nbn_guid : {
+    type: String,
+    required: true,
+    unique : true
+  },
   common_name : {
     type: String,
     required: true,
@@ -12,6 +32,23 @@ const BirdSchema = new Schema({
     required : true,
     unique : true
   },
+  family : {
+    type: String,
+    required: false,
+    unique : false
+  },
+  establishment_means : {
+    type: String,
+    required: false,
+    unique : false
+  },
+  establishment_status : {
+    type: String,
+    required: false,
+    unique : false
+  },
+  habitats : [],
+  other_common_names : [],
   images : [],
   sounds : []
 });

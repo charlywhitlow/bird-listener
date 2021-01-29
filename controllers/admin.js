2aea45cd73335a65b20704d8e1f35bc70f72a2b2
Charly Whitlow <charlywhitlow@gmail.com>
2021-01-29 14:23:47 +0000

    deleted nbn functions and data

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,6 @@ const BirdModel = require('../models/birdModel');
 const UserModel = require('../models/userModel');
 
 const birdsJSON = 'data/birds.json';
-const birdsCSV = 'data/NBN_bird_list.csv';
 
 
 async function getBirdsJSON(){
@@ -40,15 +39,6 @@ async function createBirdsJSON(){
 
     // create new birds.json from csv
     let birds_json = csvToJson.fieldDelimiter(',').getJsonFromCsv(birdsCSV);
-
-    // add additional fields from NBN:
-    birds_json = await addNBNFields(birds_json);
-    if (birds_json.error) {
-        return {
-            "message": "error extracting NBN data",
-            "error": error
-        }
-    }
 
     // write updated json to file
     let written = await writeJSON(birds_json);
@@ -108,7 +98,6 @@ async function archiveBirdsJSON(){
 
     let timestamp = await timeStamp();
     let archive_path = 'data/archive/birds_'+timestamp+'.json';
-    console.log(archive_path)
 
     return new Promise((resolve, reject) => {
         if (fs.existsSync(birdsJSON)) {
@@ -127,55 +116,6 @@ async function archiveBirdsJSON(){
     })
 }
 
-async function addNBNFields(birds_json){
-
-    // loop through species list and populate json from NBN species lookup
-    for(let i=0; i<birds_json.length; i++){
-        let bird = await getNBNSpeciesData(birds_json[i].nbn_guid);
-        for (let [key, value] of Object.entries(bird)) {
-            birds_json[i][key] = value;
-        }
-    }
-    return birds_json;
-}
-
-async function getNBNSpeciesData(nbn_guid){
-
-    let bird_json = {
-        "common_name" : "",
-        "other_common_names" : [],
-        "habitats" : [],
-        "establishment_means" : "",
-        "establishment_status" : ""
-    };
-
-    await unirest
-    .get(`https://species-ws.nbnatlas.org/species/${nbn_guid}.json`)
-    .headers({
-        'Accept': 'application/json', 
-        'Content-Type': 'application/json'
-    })
-    .then(response => {
-        let bird = response.body;
-        for (let i=0; i<bird.commonNames.length; i++){
-            if (bird.commonNames[i].language === 'en'){
-                if (bird.commonNames[i].status === 'preferred'){
-                    bird_json.common_name = bird.commonNames[i].nameString;
-                }else{
-                    bird_json.other_common_names.push(bird.commonNames[i].nameString)
-                }
-            }
-        }
-        bird_json.habitats = bird.habitat_m_s;
-        bird_json.establishment_means = bird.establishmentMeans;
-        bird_json.establishment_status = bird.establishmentStatus_s;
-    })
-    .catch(err => {
-        return {error: err};
-    });
-
-    return bird_json;
-}
 
 async function writeJSON(bird_json){
     return new Promise((resolve, reject) => {

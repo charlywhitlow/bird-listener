2af90e4b43b3d970ceb32c496d91227450fad636
Charly Whitlow <charlywhitlow@gmail.com>
2021-02-03 16:39:32 +0000

    functions for getting wikimedia fields from url

---
+++ controllers/wikimedia.js
@@ -0,0 +1,54 @@
+const unirest = require('unirest');
+
+// get required fields for given wikimedia info page url
+async function getImageInfo(image_url){
+
+    let query_url = "https://en.wikipedia.org/w/api.php?" + 
+        "action=query&prop=imageinfo&iiprop=extmetadata&titles=" +
+        getFilenameFromURL(image_url) + 
+        "&format=json"
+
+    let response = await wikimediaRequest(query_url);
+    
+    let page_key = Object.keys(response.query.pages)[0];
+    let selectedFields = getSelectedFields(response.query.pages[page_key].imageinfo[0].extmetadata);
+
+    return selectedFields;
+}
+
+// get required database fields from imageInfo json
+function getSelectedFields(imageInfo){
+    return {
+        'image_author' : imageInfo.Artist.value,
+        'image_license_code' : imageInfo.LicenseShortName.value,
+        'image_license_url' : imageInfo.LicenseUrl.value
+    }
+}
+// get image filename from wikimedia image URL
+function getFilenameFromURL(url){
+    let split1 = url.split('#')
+    let split2 = split1[0].split('/wiki/')
+    let filename = split2[1];
+    return filename;
+}
+// handle api request
+async function wikimediaRequest(url){
+    return new Promise((resolve, reject) => {
+        unirest.get(url)
+        .headers({
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json'
+        })
+        .end(function (response) {
+            if (response.error) {
+                return reject(response.error)
+            }
+            return resolve(response.body);
+        });
+    })
+}
+
+module.exports = getImageInfo;
+
+// let url = "https://commons.wikimedia.org/wiki/File:Eurasian_blue_tit_Lancashire.jpg#mw-jump-to-license"
+// getImageInfo(url)

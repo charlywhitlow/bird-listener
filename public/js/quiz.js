a29dcdf95dd778c2d8354bfeb83bc3c8561f8676
Charly Whitlow <charlywhitlow@gmail.com>
2021-02-09 16:59:49 +0000

    updated quiz with new db fields

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -26,47 +26,48 @@ function showErrorPage(error) {
     document.getElementById("loader").style.display = "none";
     document.getElementById("content").style.display = "none";
 }
-
-function playSoundButton(){
-    // play audio, or pause if already playing
-    if (document.getElementById("player").paused) { document.getElementById("player").play();}
-    else document.getElementById("player").pause();
-
-    // add 'reveal bird' button if quiz answer not already visible
-    if(document.getElementById("quiz-answer").style.display == "none"){
-        document.getElementById("reveal-bird-button").style.display = "block";        
-    }
+function goToQuizReview(){
+    window.location.replace("quiz-gallery");
 }
-async function revealBirdButton(){
-    document.getElementById("listen").style.display = "none";
-    document.getElementById("quiz-answer").style.display = "block";
-    document.getElementById("reveal-bird-button").style.display = "none";
-
-    // show 'next' button- unless moving to bird 10 (last bird)
+// function playSoundButton(){
+//     // play audio, or pause if already playing
+//     if (document.getElementById("player").paused) { document.getElementById("player").play();}
+//     else document.getElementById("player").pause();
+//     // add 'reveal bird' button if quiz answer not already visible
+//     if(document.getElementById("quiz-answer").style.display == "none"){
+//         document.getElementById("reveal-bird-button").style.display = "block";        
+//     }
+// }
+async function revealBird(){
+    document.getElementById('reveal-button').style.display = "none";
+    document.getElementById('common-name').style.visibility = "visible";
+    document.getElementById('scientific-name').style.visibility = "visible";
+    document.getElementById('image-license-panel').style.visibility = "visible";
+    document.getElementById('bird-image').style.display = "";
+    // show 'review' button when on last bird (bird 10)
     let page = parseInt(document.getElementById("page").innerHTML, 10);
     if (page == 10) {
         document.getElementById("next-button").style.display = "none";
-        document.getElementById("finish-block").style.display = "block";
+        document.getElementById("review-button").style.display = "";
+    }else{
+        document.getElementById('next-button').style.visibility = "visible";    
     }
 };
-async function nextBirdButton(){
-    document.getElementById("player").pause(); // stop audio if playing
-    document.getElementById("listen").style.display = "block";
-    document.getElementById("quiz-answer").style.display = "none";
-    document.getElementById("reveal-bird-button").style.display = "none";
-    
+async function showNextBird(){
+    document.getElementById('audio-player').pause(); // stop audio if playing
+    document.getElementById('reveal-button').style.display = "";
+    document.getElementById('common-name').style.visibility = "hidden";
+    document.getElementById('scientific-name').style.visibility = "hidden";
+    document.getElementById('image-license-panel').style.visibility = "hidden";
+    document.getElementById('bird-image').style.display = "none";
+    document.getElementById("next-button").style.visibility = "hidden";
     // increment page- up to page 10 (last bird)
     let page = parseInt(document.getElementById("page").innerHTML, 10);
     document.getElementById("page").innerHTML = page+1;
-
     // preload next bird
     let nextBird = await getNextBird();
     updateBirdFields(nextBird);
 }
-async function quizGalleryButton(){
-    window.location.replace("quiz-gallery"); // navigate to gallery view
-}
-
 // returns value of given cookie name if it exists
 function getCookie(cookie_name) {
     var name = cookie_name + "=";
@@ -97,18 +98,26 @@ async function getNextBird(){
     .catch(err => {
         console.log('Request Failed', err);
     });
-    
-    // handle response
     let json = await response.json();
     return json.nextBird;
 }
+// update bird in dom
 function updateBirdFields(nextBird){
     document.getElementById("common-name").innerHTML = nextBird.common_name;
     document.getElementById("scientific-name").innerHTML = nextBird.scientific_name;
-    document.getElementById("recordist").innerHTML = nextBird.sound_recordist;
+    // audio
+    document.getElementById("audio-player").setAttribute('src', nextBird.sound_url);
+    document.getElementById("sound-recordist").innerHTML = nextBird.sound_recordist;
+    document.getElementById("sound-info").href = 'https://www.xeno-canto.org/'+nextBird.xeno_id;
+    document.getElementById("xeno-id").innerHTML = 'XC'+nextBird.xeno_id;
     document.getElementById("sound-license-code").innerHTML = nextBird.sound_license_code;
-    // document.getElementById("sound-license-url").href = nextBird.sound_license_url;
-    document.getElementById("player").setAttribute('src', nextBird.sound_url);
-    document.getElementById("xeno-url").href = 'https://www.xeno-canto.org/'+nextBird.xeno_id;
-    document.getElementById("xeno-url").innerHTML = 'XC'+nextBird.xeno_id;
+    document.getElementById("sound-license").href = nextBird.sound_license_url;
+    // image
+    document.getElementById("bird-image").setAttribute('src', nextBird.image_url);
+    document.getElementById("image-url").href = nextBird.image_url;
+    document.getElementById("image-author").innerHTML = nextBird.image_author;
+    document.getElementById("image-license").innerHTML = nextBird.image_license_code;
+    document.getElementById("image-license").href = nextBird.image_license_url;
+    let objectPosition = nextBird.image_css_x+"% "+nextBird.image_css_y+"%";
+    document.getElementById('bird-image').style.objectPosition = objectPosition;
 }

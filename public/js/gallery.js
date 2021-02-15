b008ef6e970edb15ecb7126a22736721ffd2a319
Charly Whitlow <charlywhitlow@gmail.com>
2021-02-15 13:02:39 +0000

    added search by common name input

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -21,3 +21,16 @@ function sortZA(){
         birds[i].style.order = -i;
     }
 }
+
+function filterBirds(str){
+    // Loop through birds and hide those that don't match the search query
+    let birds = document.getElementsByClassName('bird-panel');
+    for (i = 0; i < birds.length; i++) {
+        let commonName = birds[i].getElementsByClassName("common-name")[0].innerHTML;
+        if (commonName.toLowerCase().indexOf(str.toLowerCase()) > -1) {
+            birds[i].style.display = "";
+        } else {
+            birds[i].style.display = "none";
+        }
+    }
+}

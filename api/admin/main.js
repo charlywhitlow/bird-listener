0b183c95198310d68478d96fb48d776f88019c2f
Charly Whitlow <charlywhitlow@gmail.com>
2021-02-17 16:08:16 +0000

    added admin dashboard page/route

--- api/admin/main.js
+++ api/admin/main.js
@@ -10,6 +10,11 @@ router.get('/api/status', (req, res, next) => {
 	});
 });
 
+// admin dashboard
+router.get('/admin/dashboard', function (req, res) {
+    res.render('admin/dashboard', {layout: false});
+});
+
 // object-position
 router.get('/admin/object-position', function (req, res) {
     res.render('admin/object-position', {layout: false});

5a0f1612f3484895b9a279515f4f2f395cc3b899
Charly Whitlow <charlywhitlow@gmail.com>
2021-02-18 16:34:15 +0000

    added admin routes and page outlines

--- api/admin/main.js
+++ api/admin/main.js
@@ -15,6 +15,21 @@ router.get('/admin/dashboard', function (req, res) {
     res.render('admin/dashboard', {layout: false});
 });
 
+// add birds
+router.get('/admin/add-birds', function (req, res) {
+    res.render('admin/add-birds', {layout: false});
+});
+
+// add sounds
+router.get('/admin/add-sounds', function (req, res) {
+    res.render('admin/add-sounds', {layout: false});
+});
+
+// add images
+router.get('/admin/add-images', function (req, res) {
+    res.render('admin/add-images', {layout: false});
+});
+
 // object-position
 router.get('/admin/object-position', function (req, res) {
     res.render('admin/object-position', {layout: false});

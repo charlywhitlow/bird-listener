94043e68d9ad71dce84ab23520a4c13ea5a97833
Charly Whitlow <charlywhitlow@gmail.com>
2021-03-04 15:52:30 +0000

    removed old admin controller & renamed admin-util

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,246 +1,124 @@
-const fs = require('fs');
-const csvtoJson = require('csvtojson');
-const BirdModel = require(__root + '/models/birdModel');
-const UserModel = require(__root + '/models/userModel');
-const xeno = require(__root + '/controllers/xeno-canto.js');
-const wiki = require(__root + '/controllers/wikimedia.js');
-const fileFunctions = require(__root + '/util/fileFunctions.js');
+const unirest = require('unirest');
+const HTMLParser = require('node-html-parser');
+const puppeteer = require('puppeteer');
 
-const birdsJSON = __root + 'data/birds.json';
-const birdsCSV = __root + 'data/birds.csv';
-
-
-// 1. create/update bird.json from birds.csv (creates backup of existing birds.json first)
-async function createBirdsJSONFromCSV(csvFilePath=birdsCSV, jsonFilePath=birdsJSON){
-
-    // load birds.csv into json
-    let json = await csvtoJson().fromFile(csvFilePath);
-
-    // archive existing birds.json file
-    let archived = await fileFunctions.archiveFile(jsonFilePath);
-    if (archived.error){
-        return {
-            "message": "error archiving file",
-            "error": archived.error
+function toProperCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
         }
-    }
-
-    // write json to file
-    let written = await fileFunctions.writeJSON(json, jsonFilePath);
-    if (written.error){
-        return {
-            "message": "error writing json",
-            "error": written.error
-        }
-    }
-    return {
-        "status":"ok",
-        "message":"birds.json file updated"
-    }
+    );
 }
-
-// 2. populate birds.json with image/sound info from xeno-canto / wikimedia
-async function updateBirdsJsonAndCsv(csvFilePath=birdsCSV, jsonFilePath=birdsJSON){
-
-    // load existing birds.json file
-    let json = await getBirdsJSON()
-    .then((result)=>{
-        return result.birds
-    })
-    .catch(err => {
-        console.log('error loading birds.json:')
-        console.log(err)
-        return {
-            "message": "error loading json",
-            "error": err
-        }
-    });
-
-    // add additional fields
-    for (let i=0; i<json.length; i++){
-
-        // add recording info
-        let xeno_id = json[i].xeno_id;
-        await xeno.getRecordingInfo(xeno_id)
-        .catch(err => {
-            console.log('err getting recording id '+xeno_id);
-            console.log(err)
-        })
-        .then((recording_info) => {
-            for (let [key, value] of Object.entries(recording_info)) {
-                // only update from if value doesn't exist in csv, so doesn't overwrite manual updates
-                if(!json[i][key]){
-                    json[i][key] = value;
-                }
-            }
-        });
-
-        // add image info
-        let image_info_url = wiki.extractMainImageInfoURL(json[i].image_info_url);
-        json[i].image_info_url = image_info_url;
-
-        // get image_url
-        let image_url = await wiki.getImageURL(image_info_url);
-        json[i].image_url = image_url;   
-
-        // add image author and license fields
-        await wiki.getImageInfo(image_info_url)
-        .catch(err => {
-            console.log('err getting image info '+image_info_url);
-            console.log(err)
-        })
-        .then((image_info) => {
-            for (let [key, value] of Object.entries(image_info)) {
-                // only update if value doesn't exist in csv, so doesn't overwrite manual updates
-                if(!json[i][key]){
-                    json[i][key] = value;
-                }
-            }
-        });
-    }
-
-    // archive existing birds.json, and replace with updated json
-    let json_archived = await fileFunctions.archiveFile(jsonFilePath);
-    if (json_archived.error){
-        return {
-            "message": "error archiving file",
-            "error": json_archived.error
-        }
-    }
-    let json_written = await fileFunctions.writeJSON(json, jsonFilePath);
-    if (json_written.error){
-        return {
-            "message": "error writing json",
-            "error": json_written.error
-        }
-    }
-
-    // archive existing birds.csv, and replace with updated csv
-    let csv_archived = await fileFunctions.archiveFile(csvFilePath);
-    if (csv_archived.error){
-        console.log('error archiving csv:')
-        console.log(csv_archived.error)
-        return {
-            "message": "error archiving csv",
-            "error": csv_archived.error
-        }
-    }
-    let csv_written = await fileFunctions.writeCSV(json, csvFilePath);
-    if (csv_written.error){
-        console.log('error writing csv:')
-        console.log(csv_written.error)
-        return {
-            "message": "error writing csv",
-            "error": csv_written.error
-        }
-    }
-    return {
-        "status":"ok",
-        "message":"birds.json and birds.csv files updated"
-    }
-}
-
-// 3. build db from birds.csv
-async function buildDBFromCSV(csvFilePath=birdsCSV, jsonFilePath=birdsJSON){
-    await createBirdsJSONFromCSV(csvFilePath);
-    await buildDBFromJSON(jsonFilePath);
-
-    return {
-        "status": "ok",
-        "message": "Database and user queues updated from CSV"
-    }
-}
-// build db from birds.json
-async function buildDBFromJSON(jsonFilePath=birdsJSON){
-
-    // empty db
-    await BirdModel.deleteMany({})
-    .catch(function(err){
-        console.log('Problem clearing database');
-        console.log(err);
-    });
-    await BirdModel.cleanIndexes();
-
-    // populate db from json
-    let content = await fs.readFileSync(jsonFilePath);
-    let json = await JSON.parse(content);
-    for(let i in json) {
-        await BirdModel.create(json[i])
-        .catch(function(err){ 
-            console.log('Problem updating birds database')
-            console.log(err)
-        });
-    }
-
-    // update user queues to reflect new db
-    for await (const user of UserModel.find()) {
-        let birdQueue = await user.initQueue()
-        await UserModel.findOneAndUpdate(
-            { _id : user._id}, 
-            { birdQueue: birdQueue }, 
-            { useFindAndModify: false}
-        )
-        .catch(function(err){ 
-            console.log('Problem updating user queues');
-            console.log(err);
-        });
-    }
-    return {
-        "status": "ok",
-        "message": "Database and user queues updated from JSON"
-    }
-}
-
-// 4. return existing birds.json
-async function getBirdsJSON(jsonFilePath=birdsJSON){
+async function getResponse(url){
     return new Promise((resolve, reject) => {
-        fs.readFile(jsonFilePath, (err, data) => {
-            if (err) {
-                console.log('error:')
-                console.log(err)
-                return reject({
-                    "status":"error",
-                    "error": err
-                });
-            };
-            let json = JSON.parse(data);
-
-            return resolve({
-                "status": "ok",
-                "birds": json
-            });
+        unirest.get(url)
+        .headers({
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json'
+        })
+        .end(function (response) {
+            if (response.error) {
+                return reject(response.error)
+            }
+            return resolve(response.body);
         });
     })
 }
 
-// get rows in birds.csv with blanks for css fields (i.e. that need updating)
-async function getCssFields(csvPath=birdsCSV){
-    
-    let blanks = await csvtoJson({
-        includeColumns: 
-            /(image_url|image_css_x|image_css_y|common_name|scientific_name)/
-    }).fromFile(csvPath)
-    .then(json => {
-        let blanks = [];
-        for (let i=0; i<json.length; i++){
-            if (json[i].image_css_x === "" | json[i].image_css_x === ""){
-                blanks.push(json[i])
-            }
-        }
-        return blanks;
-    });
-    return {
-        "status": "ok",
-        "message": "Database and user queues updated from JSON",
-        "blanks": blanks
+function uploadCSV(uploadPath, req){
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return { 
+            'status' : 'failed',
+            'message' : 'No files were uploaded'
+        };
     }
+    req.files.uploadFile.mv(uploadPath, function(err) {
+        if (err) return { 
+            'status' : 'failed',
+            'message' : 'Problem uploading file'
+        }
+    });
+    return false;
 }
+function checkCSVHeaders(expectedHeaders, uploadHeaders){
+    let headersOk = true;
+    expectedHeaders.forEach(header => {
+        if (!uploadHeaders.includes(header)){
+            headersOk = false;
+        }
+    });
+    return headersOk;
+}
+
+async function getRecordingDetails(birds){
+    for(let bird of birds) {
+        await getResponse('https://www.xeno-canto.org/api/2/recordings?query=nr:'+bird.xeno_id)
+        .then(res => res.recordings[0])
+        .then(sound => {
+            bird.sound_info_url = 'https://www.xeno-canto.org/'+bird.xeno_id;        
+            bird.sound_recordist = toProperCase(sound.rec);
+            bird.sound_license_url = "https:"+sound.lic;
+            bird.sound_license_code = getRecordingLicenseCode("https:"+sound.lic);
+            bird.sound_url = "https:" + sound.sono.small.split("ffts")[0] + sound['file-name'];
+        })
+        .catch(err => console.log(err))
+    }
+    return birds;
+}
+function getRecordingLicenseCode(license_url){
+    let split = license_url.split('licenses/');
+    let elements = split[1].split('/');
+    return elements[0].toUpperCase()+" "+elements[1]
+}
+
+async function getImageDetails(birds){
+    for(let bird of birds) {
+        console.log(`getting image info for ${bird.common_name}...`)
+        bird.image_info_url = bird.image_info_url.split('#')[0]; // remove any #section bookmarks
+        bird.image_url = await getImageURL(bird.image_info_url); // use puppeteer to extract image_url
+        let filename = bird.image_info_url.split('/wiki/')[1];
+        let wikimedia_query_url = "https://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&iiprop=extmetadata&titles=" 
+            + filename + "&format=json"
+
+        await getResponse(wikimedia_query_url)
+        .then(res => {
+            let page_key = (Array.isArray(Object.keys(res.query.pages))) ?
+                Object.keys(res.query.pages)[0] :
+                Object.keys(res.query.pages);
+            if ('imageinfo' in res.query.pages[page_key]){
+                return res.query.pages[page_key].imageinfo[0].extmetadata;
+            }
+        }).then(info => {
+            bird.image_author_raw = (info.Artist) ? 
+                info.Artist.value : '';
+            bird.image_author = (info.Artist) ? 
+                toProperCase( HTMLParser.parse(info.Artist.value).innerText ) : '';
+            bird.image_license_code = (info.LicenseShortName) ?
+                info.LicenseShortName.value : '';
+            bird.image_license_url = (info.LicenseUrl) ? 
+                info.LicenseUrl.value : '';
+        })
+        .catch(err => console.log(err))
+    }
+    console.log('images loaded')
+    return birds;
+}
+async function getImageURL(image_info_url) {
+    const browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await page.goto(image_info_url, {waitUntil: 'load'});
+    const image_url = await page.evaluate(() => document.querySelector("#file > a").href)
+    await browser.close();
+    return image_url;
+};
 
 
 module.exports = {
-    getBirdsJSON,
-    createBirdsJSONFromCSV,
-    updateBirdsJsonAndCsv,
-    buildDBFromCSV,
-    getCssFields
+    uploadCSV,
+    checkCSVHeaders,
+    getRecordingDetails,
+    getImageDetails,
+    toProperCase,
+    getResponse
 }

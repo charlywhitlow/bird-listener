bc68a6817b2f398d33d1de1067aedc5b63c6bc54
Charly Whitlow <charlywhitlow@gmail.com>
2021-03-22 13:40:50 +0000

    added numResults & reset button + hide load-more

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -147,8 +147,20 @@ function initAutocomplete(input, submit, searchTerms) {
     });
 
     function searchBirds(str){
+        // if search str empty, do nothing
+        if (str.length == 0){
+            return;
+        }
+        let matches = getMatches(str);
+
+        // update num results
+        document.getElementById('results').style.display="";
+        document.getElementById('numResults').innerHTML = matches.length;
+
+        // hide 'load more' button
+        document.getElementById('loadMoreButton').style.visibility="hidden";
+
         // loop through birds in dom, hide if not a match
-        let matches = getMatches(str)
         let birds = document.getElementsByClassName('bird-panel');
         for (i = 0; i < birds.length; i++) {
             let index = matches.indexOf(birds[i].id);
@@ -159,17 +171,19 @@ function initAutocomplete(input, submit, searchTerms) {
                 matches.splice(index, 1); // remove from matches array
             }
         }
-        // for matches not in dom, get bird from db and append to dom
-        fetch('/api/birds/load-matching-birds', {
-            method: "POST",
-            body: JSON.stringify(matches),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-        })
-        .then(res => res.json())
-        .then(json => {
-            appendBirdsToDom(json.birds)
-        })
-        .catch(err => console.log('Request Failed', err));
+        // for any remaining matches not in dom, get bird from db and append to dom
+        if (matches.length > 0){
+            fetch('/api/birds/load-matching-birds', {
+                method: "POST",
+                body: JSON.stringify(matches),
+                headers: {"Content-type": "application/json; charset=UTF-8"}
+            })
+            .then(res => res.json())
+            .then(json => {
+                appendBirdsToDom(json.birds)
+            })
+            .catch(err => console.log('Request Failed', err));
+        }
     }
 
     function getMatches(str){
